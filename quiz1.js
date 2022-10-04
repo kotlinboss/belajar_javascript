8bf400c5463535b382f71ba9739152dd026b45fb
jonry_kw <referbaru1997@gmail.com>
2022-10-04 14:21:33 +0700

    mengerjakan quiz pertama dan berhasil

---
+++ quiz1.js
@@ -0,0 +1,23 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+
+let firstName = "jonry"
+let lastName = "kw"
+let age = 2500
+let isMarried = 2
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+module.exports = {
+  firstName, lastName, age, isMarried,
+};
