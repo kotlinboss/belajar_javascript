9e073085abeacdeaa8143637736ffa75c8ae218f
kotlinboss <referbaru1997@gmail.com>
2022-10-19 21:47:19 +0700

    configure array.js

--- Struktur_Data_Javascript/Teory/array.js
+++ Struktur_Data_Javascript/Teory/array.js
@@ -20,25 +20,25 @@ console.log(laptop)
 console.log("\n===== menghapus data di awal array =====")
 // menghapus data di awal
 laptop.shift()
-console.log(laptop)
+console.log(laptop) // [ 'Macbook', 'Asus', 'Acer' ]
 
 console.log("\n===== menambah data di awal array =====")
 // menambah data di awal
 laptop.unshift("Dell")
-console.log(laptop)
+console.log(laptop) // [ 'Dell', 'Macbook', 'Asus', 'Acer' ]
 
 
 console.log("\n=== menghapus data ke-3(asus) pada array ===")
 console.log("=== meninggalkan keterangan <1 empty item> ===")
-// menambah data di awal
+// menghapus data ke 3
 delete laptop[2]
-console.log(laptop)
+console.log(laptop) // [ 'Dell', 'Macbook', <1 empty item>, 'Acer' ]
 
 console.log("\n=== menghapus data ke-3(asus) pada array ===")
 console.log("TANPA meninggalkan keterangan <1 empty item>")
-// menambah data di awal
+
 laptop.splice(1,2) // menghapus index ke 1 sebanyak 2 elemen
-console.log(laptop)
+console.log(laptop) // [ 'Dell', 'Acer' ]
 
 
 
